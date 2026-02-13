40f11fa442d810d8728179bcbdee6b7f1bd7f00b
HyperM0d <stw.mick@gmail.com>
2026-02-13 22:28:39 +0100

    Create buffer.py

---
+++ chess_ppo/buffer.py
@@ -0,0 +1,26 @@
+# trajectory storage for ppo update
+# https://stackoverflow.com/questions/50618214/proximal-policy-optimization-pseudo-code-vs-code
+class buffer:
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.log_probs = []
+        self.dones = []
+    
+    def add(self, state, action, reward, value, log_prob, done):
+        self.states.append(state)
+        self.actions.append(action)
+        self.rewards.append(reward)
+        self.values.append(value)
+        self.log_probs.append(log_prob)
+        self.dones.append(done)
+    
+    def clear(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.values = []
+        self.log_probs = []
+        self.dones = []
